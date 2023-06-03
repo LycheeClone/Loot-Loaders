b48425d6f25e851adb380f1678da5f44431327fe
İbrahim Halil Demir <code.halildemir@gmail.com>
2023-06-03 17:58:10 +0300

    Ongoing Development and Improvements

---
+++ Assets/Scripts/EndlessRoadController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessRoadController : MonoBehaviour
+{
+
+    public float speed;
+
+    public Transform[] roads;
+    
+    
+
+
+}
---
+++ Assets/Scripts/draft.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
+
+
+public class draft : MonoBehaviour
+{
+    public GameObject CarToSpawn;
+    public Rigidbody Rb;
+    public float Speed;
+
+    private void Awake()
+    {
+        CarToSpawn = GameObject.FindGameObjectWithTag("CrossCar");
+    }
+
+    private void Update()
+    {
+        //rb.AddForce(0,0,-speed*Time.deltaTime);
+        transform.Translate(0, 0, Speed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        if (gameObject.transform.position.z <= -8)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
